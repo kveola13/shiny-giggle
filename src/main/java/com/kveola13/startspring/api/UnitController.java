c5e92f4d691d51aba2677d8cdf4d6f2d992e5ab8
olakv <kveola13@student.westerdals.no>
2020-05-31 00:46:15 +0200

    added annotations

--- src/main/java/com/kveola13/startspring/api/UnitController.java
+++ src/main/java/com/kveola13/startspring/api/UnitController.java
@@ -2,14 +2,19 @@ package com.kveola13.startspring.api;
 
 import com.kveola13.startspring.model.Unit;
 import com.kveola13.startspring.service.UnitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UnitController {
     private final UnitService unitService;
 
+    @Autowired
     public UnitController(UnitService unitService) {
         this.unitService = unitService;
     }
-
+    @PostMapping
     public void addUnit(Unit unit) {
         unitService.addUnit(unit);
     }
