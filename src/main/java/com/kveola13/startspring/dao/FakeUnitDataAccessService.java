ec0d3f943014c4faf11b002dfdeb3583571bc350
olakv <kveola13@student.westerdals.no>
2020-05-30 14:28:38 +0200

    added dao

---
+++ src/main/java/com/kveola13/startspring/dao/FakeUnitDataAccessService.java
@@ -0,0 +1,17 @@
+package com.kveola13.startspring.dao;
+
+import com.kveola13.startspring.model.Unit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FakeUnitDataAccessService implements UnitDao {
+
+    private static List<Unit> database = new ArrayList<>();
+
+    @Override
+    public int insertUnit(UUID id, Unit unit) {
+        return 0;
+    }
+}
